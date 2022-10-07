53e4fde2c4e5b467db6ca58a0a3f196604246341
SnowUnix <56621824+SnowUnix@users.noreply.github.com>
2022-10-07 03:12:59 -0700

    Update serverhop.lua

--- Misc/serverhop.lua
+++ Misc/serverhop.lua
@@ -1,7 +1,6 @@
 local Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. "13822889" .. '/servers/Public?sortOrder=Asc&limit=100'))
 local Cursor = "none"
 local TableIds = {}
-local x = 1
 
 Site = game:GetService("HttpService"):JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(game.PlaceId) .. '/servers/Public?sortOrder=Asc&limit=100'))
 
